21b1313e50cf851a77dc798a9e4f74a7b71683f8
liuxinjie@123 <liuxinjie@yimei180.com>
2016-10-10 09:43:24 +0800

    controller

--- src/main/java/com/dream/config/ThymeleafConfigure.java
+++ src/main/java/com/dream/config/ThymeleafConfigure.java
@@ -1,12 +1,14 @@
 package com.dream.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
+@Configuration
 public class ThymeleafConfigure {
 
     @Bean
